c9be48003c899bee30ccf267d0b23d90228e9477
sounroy <knowsouravroy@gmail.com>
2022-12-02 15:21:14 +0530

    Add files via upload

---
+++ FileUploadNotification/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Azure.Devices;
+
+namespace FileUploadNotificationSample
+{
+    class Program
+    {
+        static ServiceClient serviceClient;
+        static string connectionString = "{PASTE_THE IOT_HUB_CONNECTION_STRING}";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receive file upload notifications\n");
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+            ReceiveFileUploadNotificationAsync();
+            Console.WriteLine("Press Enter to exit\n");
+            Console.ReadLine();
+        }
+
+        private async static void ReceiveFileUploadNotificationAsync()
+        {
+            var notificationReceiver = serviceClient.GetFileNotificationReceiver();
+            Console.WriteLine("\nListening for file upload notification...");
+            while (true)
+            {
+                var fileUploadNotification = await notificationReceiver.ReceiveAsync();
+                if (fileUploadNotification == null) continue;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received file upload notification: {0}",
+                  string.Join(", ", fileUploadNotification.BlobName));
+                Console.ResetColor();
+                await notificationReceiver.CompleteAsync(fileUploadNotification);
+            }
+        }
+    }
+}
